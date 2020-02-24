3522bf97ba874508a2a03362c8046284c2d44f63
Klajd Karaj <klajdkaraj14@gmail.com>
2020-02-24 00:26:06 +0000

    Add page metadata and SEO component.

--- 4_data_in_gatsby/tutorial-part-four/gatsby-config.js
+++ 4_data_in_gatsby/tutorial-part-four/gatsby-config.js
@@ -7,6 +7,8 @@
 module.exports = {
   siteMetadata: {
     title: `Pandas Eating Lots`,
+    description: `A simple description about pandas eating lots...`,
+    author: `gatsbyjs and klajdd`,
   },
   plugins: [
     {
@@ -38,20 +40,7 @@ module.exports = {
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `GatsbyJS`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#6b37bf`,
-        theme_color: `#6b37bf`,
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: `standalone`,
-        icon: `src/images/icon.png`, // This path is relative to the root of the site.
-      },
-    },
     `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
   ],
 }
--- 4_data_in_gatsby/tutorial-part-four/src/templates/blog-post.js
+++ 4_data_in_gatsby/tutorial-part-four/src/templates/blog-post.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 export default ({ data }) => {
   const post = data.markdownRemark
   return (
     <Layout>
+      <SEO title={post.frontmatter.title} description={post.excerpt} />
       <div>{post.frontmatter.title}</div>
       <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
     </Layout>
@@ -19,6 +21,7 @@ export const query = graphql`
       frontmatter {
         title
       }
+      excerpt
     }
   }
 `
